839d22b26aca231b0d247e638fd525aede715543
Kat Taylor - Carpenter <53796272+CoralStar@users.noreply.github.com>
2022-08-17 16:01:08 -0500

    Fetch user data

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const overview = document.querySelector(".overview");
+const unsername = "CoralStar";
+
+const gitUserInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+};

618d66b47d594d348e3f117d4b9efb9afd3a5fb7
Kat Taylor - Carpenter <53796272+CoralStar@users.noreply.github.com>
2022-08-18 15:07:38 -0500

    Fetch Repo Data 2

--- js/script.js
+++ js/script.js
@@ -25,12 +25,22 @@ const showUserInfo = function (data) {
     </div> 
   `;
   overview.append(div);
+  grabRepos();
 };
 
 const grabRepos = async function () {
   const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   const repoData = await fetchRepos.json();
-  
+  showRepos(repoData);
 };
 
+
+const showRepos = function (repos) {
+  for (const repo of repos) {
+  const repoItem = document.createElement("li");
+  repoItem.classList.add("repo");
+  repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+  repoList.append(repoItem);
+  }
+};
 
